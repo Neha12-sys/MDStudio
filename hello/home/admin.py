ab657b8ead817e51362345f7b9d7d0373a3ac2b4
Neha12-sys <68549619+Neha12-sys@users.noreply.github.com>
2021-04-10 21:13:57 +0530

    Update admin.py

--- hello/home/admin.py
+++ hello/home/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from Home.models import Contact
+from home.models import Chat
+from home.models import Chat1
 # Register your models here.
+admin.site.register(Chat)
+admin.site.register(Chat1)
 admin.site.register(Contact)
