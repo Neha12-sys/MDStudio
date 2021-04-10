6260c37eda441d6886ac3d50cead281196f5c33f
Neha12-sys <68549619+Neha12-sys@users.noreply.github.com>
2021-04-10 21:16:23 +0530

    Update views.py

--- hello/home/views.py
+++ hello/home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from Home.models import Contact
+from home.models import Chat
+from home.models import Chat1
 
 # Create your views here.
-
-
 def index(request):
     return render(request, 'index.html')
 
@@ -28,3 +28,22 @@ def resources(request):
 
 def MConnect(request):
    return render(request, 'MConnect.html')
+
+def chat(request):
+    if request.method == "POST":
+        email1 = request.POST.get ('email1')
+        email2 = request.POST.get ('email2')
+        email3 = request.POST.get ('email3')
+        email4 = request.POST.get ('email4')
+        chat = Chat(email1=email1,email2=email2,email3=email3,email4=email4)
+        chat.save()
+        
+    return render(request, 'Chat.html')
+
+def chat1(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        chat1 = Chat1(email=email)
+        chat1.save()
+    return render(request, 'chat1.html')
+
