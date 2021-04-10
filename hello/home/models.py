0aad06e08e3af0f04865a83c7025b320ac3806d0
Neha12-sys <68549619+Neha12-sys@users.noreply.github.com>
2021-04-10 21:14:44 +0530

    Update models.py

--- hello/home/models.py
+++ hello/home/models.py
@@ -5,3 +5,11 @@ class Contact(models.Model):
     email = models.CharField(max_length=122)
     query = models.TextField()
     date = models.DateField()
+class Chat(models.Model):
+    email1 = models.CharField(max_length=122,null=True,blank=True)
+    email2 = models.CharField(max_length=122,null=True,blank=True)
+    email3 = models.CharField(max_length=122,null=True,blank=True)
+    email4 = models.CharField(max_length=122,null=True,blank=True)
+class Chat1(models.Model):
+    email = models.CharField(max_length=122,null=True,blank=True)
+    
